49642518c6a1d3f75799c36714cb0ea2e65b7b77
HuyGia0310 <huyphamgia03102004@gmail.com>
2026-03-14 13:37:37 +0700

    bài TH2_2
    hoàn thiện ứng dụng

--- BaiTH2/app/src/main/java/com/example/appcong/MainActivity.java
+++ BaiTH2/app/src/main/java/com/example/appcong/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.appcong;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+//dây là bộ lắng nghe và xử ly sk click lên nút tính tổng
+    public void XuLyCong(View view){
+        //tìm,tham chiếu đến dữ liệu trên tệp XML
+        EditText editTextsoA = findViewById(R.id.edtA);
+        EditText editTextsoB = findViewById(R.id.edtB);
+        EditText editTextKetQua = findViewById(R.id.edtKQ);
+//lấy dữ liệu về ô điều khiển số a
+        String strA= editTextsoA.getText().toString();
+//lấy dữ liệu về ô điều khiển số b
+        String strB= editTextsoB.getText().toString();
+
+//chuyền dl sang dạng số
+        int so_A = Integer.parseInt(strA);
+        int so_B = Integer.parseInt(strB);
+//tính toán theo yc
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);
+//hiện ra mh
+        editTextKetQua.setText(strTong);
+
     }
 }
