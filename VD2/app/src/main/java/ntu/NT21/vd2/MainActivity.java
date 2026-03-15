63150b85dfcd705ddf6afdfdaf5bba464f18e929
HuyGia0310 <huyphamgia03102004@gmail.com>
2026-03-15 15:41:18 +0700

    BaiTH4(p2)
    Ứng dụng Các phép toán số học (p2. Nghe và Xử lý sự kiện OnClick XML)

--- VD2/app/src/main/java/ntu/NT21/vd2/MainActivity.java
+++ VD2/app/src/main/java/ntu/NT21/vd2/MainActivity.java
@@ -1,24 +1,103 @@
 package ntu.NT21.vd2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editTextSo1;
+    EditText editTextSo2;
+    EditText editTextKQ;
+    Button nutCong,nutTru,nutNhan,nutChia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        TimDieuKhien();
+    }
+    void TimDieuKhien(){
+        EditText editTextSo1 = (EditText)findViewById(R.id.edtSo1);
+        EditText editTextSo2 = (EditText)findViewById(R.id.edtSo2);
+        EditText editTextKQ = findViewById(R.id.edtKetQua);
+        nutCong = (Button) findViewById(R.id.btnCong);
+        nutTru = (Button) findViewById(R.id.btnTru);
+        nutNhan = (Button) findViewById(R.id.btnNhan);
+        nutChia = (Button) findViewById(R.id.btnChia);
+
+    }
+
+    public void XuLyCong(View v){
+    //tìm edtSo1 va So2
+    //lấy dữ liệu từ 2 dkhien đó
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+    //chuyển dữ liệu từ chuỗi sang số
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+    //tính toán
+        float Tong = soA + soB;
+    //hiện kq
+    //chuẩn bị dữ liệu xuất biến thành dạng chuỗi
+        String chuoiKQ = String.valueOf(Tong);
+    //gán kq lên dk
+        editTextKQ.setText(chuoiKQ);
+    }
+    public void XuLyTru(View v){
+        //tìm edtSo1 va So2
+        //lấy dữ liệu từ 2 dkhien đó
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tính toán
+        float Hieu = soA - soB;
+        //hiện kq
+        //chuẩn bị dữ liệu xuất biến thành dạng chuỗi
+        String chuoiKQ = String.valueOf(Hieu);
+        //gán kq lên dk
+        editTextKQ.setText(chuoiKQ);
+    }
+    public void XuLyNhan(View v){
+        //tìm edtSo1 va So2
+
+        //lấy dữ liệu từ 2 dkhien đó
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tính toán
+        float Tich = soA * soB;
+        //hiện kq
+
+        //chuẩn bị dữ liệu xuất biến thành dạng chuỗi
+        String chuoiKQ = String.valueOf(Tich);
+        //gán kq lên dk
+        editTextKQ.setText(chuoiKQ);
+    }
+    public void XuLyChia(View v){
+//tìm edtSo1 va So2
+
+        //lấy dữ liệu từ 2 dkhien đó
+        String soThu1 = editTextSo1.getText().toString();
+        String soThu2 = editTextSo2.getText().toString();
+        //chuyển dữ liệu từ chuỗi sang số
+        float soA = Float.parseFloat(soThu1);
+        float soB = Float.parseFloat(soThu2);
+        //tính toán
+        float Thuong = soA + soB;
+        //hiện kq
+        //chuẩn bị dữ liệu xuất biến thành dạng chuỗi
+        String chuoiKQ = String.valueOf(Thuong);
+        //gán kq lên dk
+        editTextKQ.setText(chuoiKQ);
     }
 }
